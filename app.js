7727fb7fc21c89957273a90c01f3db23e9848ac9
sumeetkhastgir <sumeetkhastgir@gmail.com>
2023-08-19 06:44:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello node deploy throgh jenkinsasas !');
+  res.send('Hello node deploy throgh  !');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
